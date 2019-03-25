b211d13eb7f1508ad8d04e1463f0993214843684
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-03-25 11:57:55 -0300

    probelma do botão do menu

--- my-app/src/BotaoMenu.js
+++ my-app/src/BotaoMenu.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react'
 import './BotaoMenu.css'
 
 class BotaoMenu extends Component {
-  //var cores = this.props.cor
-  //const coresDef = { color: #f27b81 }
-
-
-  render() {
+  render(props) {
     return (
       <div className='BotaoMenu'>
-        <a>{this.props.nome}</a>
+        
       </div>
     );
   }
