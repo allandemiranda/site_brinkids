e6c2395cc7fda0ce4bf28b6e0349ce7a0c13615d
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-04-04 16:17:54 -0300

    slideshow feito

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
-import SlideShow from './main/SlideShow'
+import Slider from './main/Slider';
+import './main/Slideshow.css';
+
+const content = [
+	{
+		image: 'http://brinkidsonline.com.br/novosite/img/slide1.png'
+	},
+	{
+		image: 'http://brinkidsonline.com.br/novosite/img/slide2.png'
+	},
+	{
+		image: 'http://brinkidsonline.com.br/novosite/img/slide3.png'
+	}
+];
 
 class Main extends Component{
     render() {
         return (
-            <SlideShow></SlideShow>
+            <Slider autoplay={3000}>
+                {content.map((item, index) => (
+                    <div
+                        key={index}
+                        style={{ background: `url('${item.image}') no-repeat center center`, backgroundSize: '100% 100%'}}
+                    >                        
+                    </div>
+                ))}
+            </Slider>
         )
     }
 }
